2c9ba3f67697be9d240d95afdf6d727932a313f9
Ле Динь Хией <33816369+SecondSony@users.noreply.github.com>
2021-03-08 02:02:52 +0300

    Updated top artist's page

--- src/views/TopArtists.vue
+++ src/views/TopArtists.vue
@@ -1,5 +1,13 @@
 <template>
   <v-container>
-    top artists
+    <v-row no-gutters justify="center">
+      <v-card v-for="i in 30" :key="i" width="250" class="ma-2 rounded-lg">
+        <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png" aspect-ratio="1"/>
+        <v-card-title class="pa-2">Исполнитель</v-card-title>
+        <v-divider/>
+        <v-card-text class="pa-2 pb-0 text-caption">Кол-во прослушиваний: ХХХ</v-card-text>
+        <v-card-text class="pa-2 pt-0 text-caption">Кол-во слушателей: ХХХ</v-card-text>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
