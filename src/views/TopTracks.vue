b80c427e56e2674704b37583df2fb53562c68aa2
Ле Динь Хией <33816369+SecondSony@users.noreply.github.com>
2021-03-18 21:02:36 +0300

    Added skeleton-loading in top artists page

--- src/views/TopTracks.vue
+++ src/views/TopTracks.vue
@@ -1,22 +1,27 @@
 <template>
   <v-container>
     <v-row no-gutters justify="center">
-      <v-card
+      <v-skeleton-loader
+        type="image, article"
+        class="ma-2 rounded-lg" 
+        width="250"
         v-for="track in topTracks"
         :key="track.artist.name + '_' + track.name"
-        width="250"
-        class="ma-2 rounded-lg"
+        :loading="loading"
       >
-        <v-img
-          :src="track.image[track.image.length-1]['#text']"
-          aspect-ratio="1"
-        />
-        <v-card-subtitle class="pa-2 pb-0">{{ track.artist.name }}</v-card-subtitle>
-        <v-card-title class="pa-2 pt-0">{{ track.name }}</v-card-title>
-        <v-divider />
-        <v-card-text class="pa-2 pb-0 text-caption">Кол-во прослушиваний: {{ track.playcount }}</v-card-text>
-        <v-card-text class="pa-2 pt-0 text-caption">Кол-во слушателей: {{ track.listeners }}</v-card-text>
-      </v-card>
+        <v-card width="250">
+          <v-img
+            :src="track.image[track.image.length-1]['#text']"
+            lazy-src="https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png"
+            aspect-ratio="1"
+          />
+          <v-card-subtitle class="pa-2 pb-0">{{ track.artist.name }}</v-card-subtitle>
+          <v-card-title class="pa-2 pt-0">{{ track.name }}</v-card-title>
+          <v-divider />
+          <v-card-text class="pa-2 pb-0 text-caption">Кол-во прослушиваний: {{ track.playcount }}</v-card-text>
+          <v-card-text class="pa-2 pt-0 text-caption">Кол-во слушателей: {{ track.listeners }}</v-card-text>
+        </v-card>
+      </v-skeleton-loader>
     </v-row>
     <infinite-loading @infinite="infiniteHandler" />
   </v-container>
@@ -27,14 +32,25 @@ export default {
   data() {
     return {
       topTracks: [],
-      page: 1
+      page: 1,
+      loading: true
     };
   },
   methods: {
     async infiniteHandler($state) {
-      let newPage = await this.$lastfm.chart.getTopTracks(this.page++);
+      let newPage = null;
       let topNames = [];
       let newNames = [];
+      this.loading = true;
+
+      await this.$lastfm.chart
+        .getTopTracks(this.page++)
+        .then((result) => {
+          newPage = result;
+          this.loading = false;
+        }).catch((err) => {
+          console.log(err.response);
+        });
 
       this.topTracks.forEach(el => topNames.push(el.name));
       newPage.forEach(el => newNames.push(el.name));
