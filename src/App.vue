08f428d9ef5cd7d83f6a272c52aef862858f07fd
Ле Динь Хией <33816369+SecondSony@users.noreply.github.com>
2021-03-20 18:11:49 +0300

    Added scroll to top page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,49 @@
 <template>
   <v-app>
     <div id="nav">
-      <router-link to="/">Топ исполнителей</router-link> | 
+      <router-link to="/">Топ исполнителей</router-link> |
       <router-link to="/tracks">Топ песен</router-link>
     </div>
-    <v-main>
-      <router-view/>
+    <v-main v-scroll="scrollHandler">
+      <v-fab-transition>
+        <v-btn
+          v-show="!hidden"
+          fab
+          fixed
+          right
+          bottom
+          icon
+          class="primary"
+          @click="scrollToTop()"
+        >
+          <v-icon x-large>mdi-chevron-up</v-icon>
+        </v-btn>
+      </v-fab-transition>
+      <router-view />
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      hidden: true,
+    };
+  },
+  methods: {
+    scrollToTop() {
+      this.$vuetify.goTo(0, {
+        ease: "easeInOutQuint",
+        duration: 300
+      });
+    },
+    scrollHandler() {
+      this.hidden = window.scrollY < 300;
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@ import InfiniteLoading from 'vue-infinite-loading'
 Vue.config.productionTip = false
 Vue.use(lastfm);
 Vue.use(InfiniteLoading);
+Vue.directive('scroll', {
+  inserted: function (el, binding) {
+    let f = function (evt) {
+      if (binding.value(evt, el)) {
+        window.removeEventListener('scroll', f);
+      }
+    }
+
+    window.addEventListener('scroll', f);
+  }
+})
 
 new Vue({
   router,
